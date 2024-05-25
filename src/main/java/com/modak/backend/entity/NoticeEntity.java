0b657d06f84682ec1064912329ed2fcdf76c62d1
conchohi <lar3242@gmail.com>
2024-05-25 16:46:30 +0900

    notice entity 수정

--- src/main/java/com/modak/backend/entity/NoticeEntity.java
+++ src/main/java/com/modak/backend/entity/NoticeEntity.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class NoticeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long noticeNo;
     private String title;
     private String content;
